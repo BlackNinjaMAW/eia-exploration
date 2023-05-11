802b6896c412aa18659fe112cc2a5b453113666d
blackninjamaw <blackninjamaw1@gmail.com>
2023-05-11 14:17:28 -0500

    Paginated API Call
    New code will iterate until every observation has been obtained from the data base.

--- scripts/query.R
+++ scripts/query.R
@@ -1,22 +1,50 @@
-library(data.table)
 library(jsonlite)
 library(magrittr)
+library(dplyr)
 library(httr)
 
-api_key <- "Put your API key here"
+api_key <- "Put your api key here"
+url <- "https://api.eia.gov/v2/electricity/retail-sales/data/?frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&sort[0][column]=period&sort[0][direction]=desc"
 
-customers <- "https://api.eia.gov/v2/electricity/retail-sales/data/?frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=" %>%
-  paste0(., api_key)
+df_customers <- data.frame()
 
-response <- GET(customers)  # Making API call
+offset <- 0
+length <- 5000
+finished <- FALSE
 
-# Check if request was successful
-if(http_status(response)$message == "OK") {
-  # Parse the received JSON data
-  dt_customers <- content(response, as = "text", encoding = "UTF-8") %>%
-    fromJSON(flatten = TRUE) %>%
-    .$response %>%
-    .$data
-} else {
-  print(paste0("Error: ", http_status(response)$message, "\nMessage Michael"))
+while(!finished) {
+  
+  # Construct the URL for the page
+  customers <-  url %>%
+    paste0(., "&api_key=", api_key) %>%
+    paste0(., "&offset=", offset) %>%
+    paste0(., "&length=", length)
+  
+  # Make the API call
+  response <- GET(customers)
+  
+  # Check if request was successful
+  if(response$status_code == 200) {
+    # Parse the JSON data
+    data <- content(response, as = "text", encoding = "UTF-8") %>%
+      fromJSON(flatten = TRUE) %>%
+      .$response %>%
+      .$data
+    
+    # Append the new data to the main data.frame
+    df_customers <- data %>%
+      rbind(df_customers)
+    
+    # If we got less data than we asked for, we've reached the end
+    if(nrow(data) < length) {
+      finished <- TRUE
+    } else {
+      # Otherwise, increase the offset for the next call
+      offset <- offset + length
+    }
+  } else {
+    # End if errant status code received
+    cat(paste(http_status(response)$message, "\nMessage Michael"))
+    finished <- TRUE
+  }
 }
