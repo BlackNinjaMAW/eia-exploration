914198d58d2695b77eab0fa61b6b5bd27f5b6c5a
DevonJStorm <devonstorm1@gmail.com>
2023-05-24 21:43:43 -0500

    Update devon_plot_script.R
    Fixed errors generating emissions plot, merged functions for totalcarbon dataframe

--- devon_plot_script.R
+++ devon_plot_script.R
@@ -4,6 +4,8 @@ library(magrittr)
 library(simts)
 library(maps)
 library(httr)
+library(ggplot2)
+library(dplyr)
 
 api_call <- function(url, api_key) {
   
@@ -59,23 +61,27 @@ api_key <- "cNBvIPbqcC8WgmDrJM5haRO9giKzNOosV1XuZgaG"
 
 emissions_url <- "https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?frequency=annual&data[0]=value&sort[0][column]=period&sort[0][direction]=desc"
 
+sector_url <- "https://api.eia.gov/v2/electricity/retail-sales/data/?frequency=monthly&data[0]=customers&data[1]=price&data[2]=revenue&data[3]=sales&sort[0][column]=period&sort[0][direction]=desc"
+
+power_url <- "https://api.eia.gov/v2/electricity/facility-fuel/data/?frequency=monthly&data[0]=average-heat-content&data[1]=consumption-for-eg&data[2]=consumption-for-eg-btu&data[3]=generation&data[4]=gross-generation&data[5]=total-consumption&data[6]=total-consumption-btu&sort[0][column]=period&sort[0][direction]=desc"
+
+df_emissions <- api_call(emissions_url, api_key)
+df_sector <- api_call(sector_url, api_key)
+df_power <- api_call(power_url, api_key)
+
 df_totalcarbon <- df_emissions %>%
   filter(sectorId == "EC") %>%
-  filter(`fuel-name` != "All Fuels")%>%
-  group_by(`fuel-name`, `state-name`)
-
-plot(plot_carbon)
-
-df_totalcarbon <- df_totalcarbon %>%
-  mutate(interval = signif(period, digits = 3))
-
+  filter(`fuel-name` != "All Fuels") %>%
+  group_by(`fuel-name`, `state-name`) %>%
+  mutate(interval = signif(period, digits = 3)) %>%
+  rename(fuel.name = "fuel-name")
 
 plot_carbon <- ggplot(data = df_totalcarbon, aes(x = period, y = value, fill = fuel.name, alpha = .5)) +
   stat_smooth(geom = "area") +
   geom_smooth(method = "lm", se = FALSE, linetype = "dashed") +
   theme(panel.grid = element_line(colour = "gray", linetype = "dashed")) +
   labs(title = "Total Carbon Emissions from Electric Power Sector by Fuel Type",
-       subtitle = "Period from 1972-2022",
+       subtitle = "Period from 1970-2020",
        x = "Period (Year)",
        y = "Metric Tons of CO2 Emitted (millions)")
 
